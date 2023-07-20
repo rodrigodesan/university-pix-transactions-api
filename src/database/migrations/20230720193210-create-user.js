b643525519d5608c34eda9fbc545f4fda5d03f8a
rodrigodesan <hiprodrigo@gmail.com>
2023-07-20 16:53:50 -0300

    feat(User): CRUD

---
+++ src/database/migrations/20230720193210-create-user.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  },
+};
---
+++ src/models/User.js
@@ -0,0 +1,63 @@
+import Sequelize from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+const { Model } = Sequelize;
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            arg: [3, 255],
+            msg: 'Name must have between 3 and 255 characters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email already exists',
+        },
+        validate: {
+          isEmail: {
+            msg: "Invalid email address",
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 12],
+            msg: 'Password must be between 6 and 12 characters',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
+  }
+
+  // static associate(models) {}
+}
